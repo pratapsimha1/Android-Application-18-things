8138050bdc78f61538a2ce5e7ca071e8b7666a1c
Prata <gkpratap92@gmail.com>
2016-04-11 23:37:27 +0530

    Initial Check in

---
+++ 18ThingsMentallyStrongPeopleDo1/src/com/example/thingsmentallystrongpeopledo1/Display.java
@@ -0,0 +1,12 @@
+package com.example.thingsmentallystrongpeopledo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Display extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display);
+	}
+
+}
